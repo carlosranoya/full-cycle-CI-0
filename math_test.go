72b259479c51dcfc178f4f20729ddae6635a983f
carlosranoya <carlosranoya@gmail.com>
2022-04-09 16:38:50 -0300

    fix: test ok

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestMDC(t *testing.T) {
 
-	mdc := MDC(15, 24)
+	mdc := MDC(15, 25)
 	var expected uint64 = 5
 	if mdc != expected {
 		t.Errorf("Resultado do MDC é inválido.\nResultado: %d\nEsperado: %d", mdc, expected)
